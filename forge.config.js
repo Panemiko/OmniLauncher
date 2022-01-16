b29731b19619b5b6c4f8ecc6e887a10a2176d4ec
Panem <titoluccao@gmail.com>
2022-01-16 16:27:46 -0300

    add forge config in a separated file

---
+++ forge.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+    packagerConfig: {
+        name: 'Omni Launcher',
+        executableName: 'omni-launcher'
+    },
+    plugins: [
+        [
+            '@electron-forge/plugin-webpack',
+            {
+                mainConfig: './webpack/webpack.main.config.js',
+                renderer: {
+                    config: './webpack/webpack.renderer.config.js',
+                    entryPoints: [
+                        {
+                            html: './public/index.html',
+                            js: './src/index.tsx',
+                            name: 'main_window'
+                        }
+                    ]
+                }
+            }
+        ]
+    ],
+    makers: [
+        {
+            name: '@electron-forge/maker-squirrel',
+            config: {
+                name: 'Omni Launcher'
+            }
+        },
+        {
+            name: '@electron-forge/maker-zip',
+            plataforms: [
+                'darwin'
+            ]
+        },
+        {
+            name: '@electron-forge/maker-deb',
+            config: {}
+        },
+        {
+            name: '@electron-forge/maker-rpm',
+            config: {}
+        }
+    ]
+}
