55b7938fcff437fa6cc545395eb8f8af4c043d88
Panem <titoluccao@gmail.com>
2022-01-17 19:47:45 -0300

    update Title bar

--- src/components/TitleBar/index.tsx
+++ src/components/TitleBar/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import { Container, WindowTitle } from './style'
+import { VscChromeMinimize, VscChromeRestore, VscChromeClose } from 'react-icons/vsc'
+import { Container, WindowTitle, IconsContainer, Icon } from './style'
 
 export default function TitleBar() {
 
-    function close() {
-        console.log('closing')
-        window.main.window.close()
-    }
+    const win = window.api.window
 
     return (
-        <>
-            <Container>
-                <WindowTitle>Omni Launcher</WindowTitle>
-            </Container>
-            <button onClick={close}>aaaaa</button>
-        </>
+        <Container>
+            <WindowTitle>Omni Launcher</WindowTitle>
+            <IconsContainer>
+                <Icon onClick={win.minimize}><VscChromeMinimize /></Icon>
+                <Icon onClick={win.maximize}><VscChromeRestore /></Icon>
+                <Icon onClick={win.close}><VscChromeClose /></Icon>
+            </IconsContainer>
+        </Container>
     )
 }
--- src/components/TitleBar/style.ts
+++ src/components/TitleBar/style.ts
@@ -1,16 +1,49 @@
 import styled from 'styled-components'
 
 export const Container = styled.header`
+    position: absolute;
+
     height: 30px;
     width: 100%;
-    background: ${props => props.theme.background};
-    -webkit-user-select: none;
+    
+    display: grid;
+    align-items: center;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-areas: '- WindowTitle IconsContainer';
+
+    background: ${props => props.theme.darkest};
+
+    -webkit-user-select: none; 
     -webkit-app-region: drag;
 `
 
 export const WindowTitle = styled.h1`
+    grid-area: WindowTitle;
+
     color: ${props => props.theme.comment};
     text-align: center;
-    font-size: 16px;
-    margin: auto;
+    font-size: 12px;
+`
+
+export const IconsContainer = styled.div`
+    height: 100%;
+    
+    grid-area: IconsContainer;
+
+    text-align: right;
+`
+
+export const Icon = styled.button`
+    height: 100%;
+    width: 35px;
+
+    background: none;
+    border: none;
+    color: ${props => props.theme.comment};
+
+    -webkit-app-region: none;
+
+    :hover {
+        background: ${props => props.theme.darker};
+    }
 `
