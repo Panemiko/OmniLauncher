ab7cdd2b80ce04dec743d1361571b9c032e7cfe4
Panem <titoluccao@gmail.com>
2022-01-16 21:01:04 -0300

    refatore loadIpcEvents

--- app/Application.ts
+++ app/Application.ts
@@ -73,17 +73,13 @@ export default class Application {
     }
 
     private async loadIpcEvents(): Promise<void> {
-        ipcMain.on('window-close', () => {
-            BrowserWindow.getFocusedWindow()?.close()
-        })
+        ipcMain.on('window-close', () => BrowserWindow.getFocusedWindow()?.close())
         ipcMain.on('window-maximize', () => {
             const currentWin = BrowserWindow.getFocusedWindow() as BrowserWindow
             if (currentWin.isMaximized()) currentWin.unmaximize()
             else currentWin?.maximize()
         })
-        ipcMain.on('window-minimize', () => {
-            BrowserWindow.getFocusedWindow()?.minimize()
-        })
+        ipcMain.on('window-minimize', () => BrowserWindow.getFocusedWindow()?.minimize())
     }
 
 }
