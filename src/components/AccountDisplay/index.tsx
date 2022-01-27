808808d5b1d1428ff39dc20953dcd11f2f796535
Panem <titoluccao@gmail.com>
2022-01-27 20:17:43 -0300

    add AccountDisplay

--- src/components/AccountDisplay/index.tsx
+++ src/components/AccountDisplay/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Container, Avatar, UsernameContainer, Username, AccountType } from './style'
 
-export default function AccountDisplay(props: AccountDisplayProps) {
+export default function AccountDisplay() {
     return (
-        <div>a</div>
+        <Container>
+            <Avatar src="avatar" />
+            <UsernameContainer>
+                <Username>Panemiko</Username>
+                <AccountType>Mojang Account</AccountType>
+            </UsernameContainer>
+        </Container>
     )
-}
-
-export interface AccountDisplayProps {
-
 }
