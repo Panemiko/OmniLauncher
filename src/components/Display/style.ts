96b15a1254ab095a8e4d4b49071926178bf76cef
Panem <titoluccao@gmail.com>
2022-01-29 23:12:13 -0300

    update Selectionlist width

--- src/components/Display/style.ts
+++ src/components/Display/style.ts
@@ -8,7 +8,7 @@ import { styled } from '@stitches/react'
 
 export const Container = styled('div', {
     display: 'grid',
-    gridTemplateColumns: 'minmax(300px, 450px) 1fr',
+    gridTemplateColumns: 'minmax(250px, 350px) auto',
     gridTemplateRows: '30px 40vh calc(60vh - 30px)',
     gridTemplateAreas: '\'TB TB\' \'SL I\' \'SL I\''
 })
